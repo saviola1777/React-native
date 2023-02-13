dca8b8f273718c381a1dd1d714e64b3db59ea8f0
Oleksandr Drahanchuk <draganchuk1991@ukr.net>
2023-02-13 21:54:44 +0200

    change

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { ImageBackground, StyleSheet, View, TouchableWithoutFeedback, Keyboard  } from 'react-native';
+import RegistrationScreen from './Screens/RegistrationScreen';
+import LoginScreen from './Screens/LoginScreen';
+
 
 export default function App() {
+  const [loginPassword, searchrState] = useState('');
+ 
+  const keyboartHide=()=>{
+    Keyboard.dismiss()
+  }
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableWithoutFeedback onPress={keyboartHide}>
+      <ImageBackground style={styles.image} source={require('./Images/phone-wallpaper-ge20dbc4f8_640.jpg')}>
+       
+       {/* <RegistrationScreen/>  */}
+       <LoginScreen/>
+       
+        <StatusBar style="auto" />
+      </ImageBackground>
+      </TouchableWithoutFeedback>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  image:{
+    flex:1,
+    resizeMode:"cover" ,
+    justifyContent:"center",
+    },  
 });
---
+++ Screens/RegistrationScreen.jsx
@@ -0,0 +1,89 @@
+
+import React, { useState } from "react";
+import { Text, View, TextInput , StyleSheet,TouchableOpacity} from 'react-native';
+
+
+
+  
+   const RegistrationScreen=  () => { 
+ 
+
+      return ( 
+      <View style={styles.form}>
+         <Text style={styles.title}>Sign up </Text>
+
+         <View style={styles.tumb}>
+    <TextInput style={styles.input}  placeholder="Login " placeholderTextColor="#ffffff" />
+    </View>
+
+    <View style={styles.tumb}>
+      
+      <TextInput style={styles.input}  placeholder="E-mail" placeholderTextColor="#ffffff"  />
+      </View>
+
+      <View style={styles.tumb} >
+      
+      <TextInput style={styles.input} placeholder="Password" placeholderTextColor="#ffffff" secureTextEntry={true} />
+      </View>
+
+      <TouchableOpacity style={styles.btn} activeOpacity={0.8}>
+        <Text style={styles.btnTitle}>Loin</Text>
+        </TouchableOpacity>
+      
+    </View>
+   )}
+   
+   const styles = StyleSheet.create({
+      title:{
+    marginBottom:30,
+    textAlign:"center",
+    fontSize:30,
+    color:"#474747",
+    
+
+        },
+        form:{
+         marginHorizontal:40,
+      },
+      tumb:{
+marginTop:15
+      },
+        imputText:{
+          color:"#1a1a1a",
+           fontSize:18,
+           marginBottom:10
+        },
+       
+      input:{
+        borderWidth:3 ,
+        borderColor:"#dbfff4",
+        padding:2  ,
+        borderRadius:5 ,
+        backgroundColor: "#393e40",
+        color:"#dbfff4" ,
+        textAlign: 'center',
+        fontSize:15 ,
+        
+      },
+      btn:{
+        
+        height:40,
+        marginTop:30,
+        borderRadius:5,
+          justifyContent: 'center',
+          alignItems: 'center',
+          ...Platform.select({
+            android:{
+              backgroundColor:"#f8f8ff",
+            
+            },
+          })
+      },
+      btnTitle:{
+        fontSize:16,
+        color:"#00bfff",
+      
+      },
+     
+   })
+   export default RegistrationScreen
